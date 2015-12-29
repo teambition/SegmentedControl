34f77b8ba8f0c0b984df2773cfee6e75a7c80842
Xin Hong <xin@teambition.com>
2015-12-29 16:58:37 +0800

    Initial Commit

---
+++ SegmentedControlExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentedControlExample
+//
+//  Created by 洪鑫 on 15/12/29.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
